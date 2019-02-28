6c67b6c70b35e662f45f18af920b0392a62a5912
Manuel Kaufmann <humitos@gmail.com>
2019-02-28 13:40:09 +0100

    Improve expandable fields using drf-flex-fields

--- readthedocs/builds/models.py
+++ readthedocs/builds/models.py
@@ -133,6 +133,10 @@ class Version(models.Model):
         )
 
     @property
+    def last_build(self):
+        return self.builds.order_by('-date').first()
+
+    @property
     def config(self):
         """
         Proxy to the configuration of the build.
